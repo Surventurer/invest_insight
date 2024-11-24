5f78b0d35dbddc0c6806de7180092ce5adb4a0e2
Surventurer <89982630+Surventurer@users.noreply.github.com>
2024-11-24 22:16:56 +0530

    v0

---
+++ streamlit_website.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import webbrowser
+import subprocess
+import os
+
+# Function to start Django server
+def start_django():
+    django_project_path = "path_to_your_django_project"  # Update this to your Django project path
+    os.chdir(django_project_path)
+    
+    # Run Django server
+    subprocess.Popen(["python", "manage.py", "runserver"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# Redirect to Django app
+st.title("Redirecting to Django Application...")
+start_django()
+webbrowser.open("http://127.0.0.1:8000")
